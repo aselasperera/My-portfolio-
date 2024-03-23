edfd3108aef02515813935da27e471267574d319
Asela S Perera <136217850+aselasperera@users.noreply.github.com>
2024-03-23 15:32:30 +0530

    Done

--- portfolio/src/Components/Home.js
+++ portfolio/src/Components/Home.js
@@ -18,7 +18,6 @@ function Portfolio() {
           <ul className="nav-links">
             <li><a href="#about">About</a></li>
             <li><a href="#experience">Experience</a></li>
-            <li><a href="#projects">Projects</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
         </div>
